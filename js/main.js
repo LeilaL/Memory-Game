97f7094c2a27fd4d3f93dff1dd8ebbb9bb5edc9c
Leila.L <leila.lababsa@hotmail.fr>
2017-07-18 10:30:07 +0200

    defined variables

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var images = [];
-
-images.sort (function()) {
-  return 0.5 - Math.random()*7)+1) ;
-} );
+var tab = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg","img/7.jpg",
+"img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg","img/7.jpg"];
+var clique=0;
+var choixun;
+var choixdeux;
