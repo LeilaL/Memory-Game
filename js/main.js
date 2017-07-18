4a95ffb94b9509b73716c10eac09934d831c25bc
Leila.L <leila.lababsa@hotmail.fr>
2017-07-18 10:35:30 +0200

    create random

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ var tab = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jp
 var clique=0;
 var choixun;
 var choixdeux;
+
+function random(tab){
+	var j, x, i;
+	for(i = tab.length; i; i--) {
+		j = Math.floor(Math.random() * i);
+		x = tab[i-1];
+		tab[i-1] = tab[j];
+		tab[j] = x;
+	}
+}
+random(tab);
