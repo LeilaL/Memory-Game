7046848e66b9998ca6051347157286aa8552f048
Leila.L <leila.lababsa@hotmail.fr>
2017-07-20 09:16:38 +0200

    ameliorate functions for choose cards

--- js/main.js
+++ js/main.js
@@ -1,50 +1,54 @@
 var tab = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg","img/7.jpg",
 "img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg","img/7.jpg"];
-var clique=0;
-var choixun;
-var choixdeux;
+var click=0;
+var choix1;
+var choix2;
+var recto = "img/index.png";
+var images = document.getElementsByTagName("img");
 
-function random(tab){
-	var j, x, i;
-	for(i = tab.length; i; i--) {
-		j = Math.floor(Math.random() * i);
-		x = tab[i-1];
-		tab[i-1] = tab[j];
-		tab[j] = x;
-	}
+// function random(tab){
+// 	var j, x, i;
+// 	for(i = tab.length; i; i--) {
+// 		j = Math.floor(Math.random() * i);
+// 		x = tab[i-1];
+// 		tab[i-1] = tab[j];
+// 		tab[j] = x;
+// 	}
+// }
+// random(tab);
+
+for (let i=0; i < images.length; i++) {
+  images[i].addEventListener('click', function () {
+    choose(i) });
 }
-random(tab);
 
-function choisir(carte) {
-	if (clique == 2) {
+function choose(carte) {
+  if (click == 2) {
 		return;
-	}
-	if (clique == 0) {
-		choixun = carte;
-		document.images[carte].src =  tab[carte];
-		document.images[choixun].style.pointerEvents = 'none';
-		clique = 1;
+}
+	if (click == 0) {
+		choix1 = tab[carte];
+		images[carte].src =  tab[carte];
+		click = 1;
 	}
 	else {
-		clique = 2;
-		choixdeux = carte;
-		document.images[carte].src =  tab[carte];
-		timer = setTimeout("verif()", 500);
+		click = 2;
+		choix2 = tab[carte];
+    images[carte].src =  tab[carte];
+		compare();
 	}
 }
 
-function verif() {
-	clique = 0;
-	if (tab[choixdeux] ==  tab[choixun]) {
-		document.images[choixun].style.pointerEvents = 'none';
-		document.images[choixun].style.opacity = '0.3';
-		document.images[choixun].style.pointerEvents = 'none';
-		document.images[choixdeux].style.opacity = '0.3';
+function compare() {
+	click = 0;
+	if (choix2 ==  choix1) {
+		// images[choix1].style.opacity = "0.5";
+		// images[choix2].style.opacity = "0.5";
+    alert('gagné');
 	} else {
-		document.images[choixun].style.pointerEvents = 'auto';
-		return;
-	}
-	if (paires==7) {
-   document.getElementsById.innerHTML ="Tu as gagné !"
+    // images[choix1].src = recto;
+    // images[choix2].src = recto;
+		// images[choix1].style.pointerEvents = "none";
+alert('perdu');
 	}
 }
