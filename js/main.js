46656cd1f6411eb16fe95a74a1034cff3a18e083
Leila.L <leila.lababsa@hotmail.fr>
2017-07-20 10:23:15 +0200

    fixed problems of index

--- js/main.js
+++ js/main.js
@@ -5,17 +5,19 @@ var choix1;
 var choix2;
 var recto = "img/index.png";
 var images = document.getElementsByTagName("img");
+var i1;
+var i2;
 
-// function random(tab){
-// 	var j, x, i;
-// 	for(i = tab.length; i; i--) {
-// 		j = Math.floor(Math.random() * i);
-// 		x = tab[i-1];
-// 		tab[i-1] = tab[j];
-// 		tab[j] = x;
-// 	}
-// }
-// random(tab);
+function random(tab){
+	var j, x, i;
+	for(i = tab.length; i; i--) {
+		j = Math.floor(Math.random() * i);
+		x = tab[i-1];
+		tab[i-1] = tab[j];
+		tab[j] = x;
+	}
+}
+random(tab);
 
 for (let i=0; i < images.length; i++) {
   images[i].addEventListener('click', function () {
@@ -23,32 +25,43 @@ for (let i=0; i < images.length; i++) {
 }
 
 function choose(carte) {
-  if (click == 2) {
-		return;
-}
+//   if (click == 2) {
+// 		// return;
+//   console.log('test');
+// }
 	if (click == 0) {
 		choix1 = tab[carte];
 		images[carte].src =  tab[carte];
 		click = 1;
+    i1 = carte;
+    // console.log("premier");
 	}
 	else {
 		click = 2;
 		choix2 = tab[carte];
     images[carte].src =  tab[carte];
-		compare();
+    i2 = carte;
+    // console.log('second');
+    compare(choix1,choix2);
 	}
 }
 
-function compare() {
+function compare(a,b) {
 	click = 0;
-	if (choix2 ==  choix1) {
-		// images[choix1].style.opacity = "0.5";
-		// images[choix2].style.opacity = "0.5";
-    alert('gagné');
+	if (a ==  b) {
+    images[a].style.opacity = "0.5";
+		images[b].style.opacity = "0.5";
+    // alert('gagné');
 	} else {
-    // images[choix1].src = recto;
-    // images[choix2].src = recto;
+    setTimeout(retourne, 1000);
+    // images[a].src = recto;
+    // images[b].src = recto;
 		// images[choix1].style.pointerEvents = "none";
-alert('perdu');
+// alert('perdu');
 	}
+}
+
+function retourne () {
+  images[i1].src= recto;
+  images[i2].src= recto;
 }
